5f1f60bbe5f03023aefeda393dd3ba9fe06314fb
MaximumFire <68478521+MaximumFire@users.noreply.github.com>
2020-10-17 22:17:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -111,4 +111,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run(os.environ['discord_token')
+client.run(os.environ['discord_token'])
