10aad3ed70f0a5e276ce8a8ab582ddfd491a2d85
MaximumFire <68478521+MaximumFire@users.noreply.github.com>
2020-10-28 16:02:28 +0000

    yolo p4 THE END hopefully

--- bot.py
+++ bot.py
@@ -48,6 +48,41 @@ async def help(ctx):
     await ctx.send("Use the '.getSteamAcc @yourNameHere' to get an account")
 
 @client.command()
+async def getSteamAcc(ctx, author):
+    global lineCount
+    global con
+    global cur
+    if "verified" in [i.name.lower() for i in ctx.author.roles]:
+        lineCount = lineCount + 1
+        print("lineCount is now: " + str(lineCount))
+        cur.execute("select * from steamAccounts")
+        tableData = cur.fetchall()
+        loop = 0
+        for row in tableData:
+            loop = loop + 1
+            if loop == lineCount:
+                if row[1] == "":
+                    await ctx.send("There are no more accounts availiable. Contact @MaximumFire for help.")
+                else:
+                    await ctx.author.send("Your username is: " + row[1])
+                    await ctx.author.send("Your password is: " + row[2])
+    else:
+        await ctx.send("This Command is only availiable for @verified")
+
+@getSteamAcc.error
+async def getSteamAcc_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        msg = 'This command has a cooldown, please try again in {:.2f}s'.format(error.retry_after)
+        await ctx.send(msg)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        if error.param.name == 'author':
+            await ctx.send("Please @ yourself after the command. For example: '.getSteamAcc @MaximumFire'")
+            getSteamAcc.reset_cooldown(ctx)
+    else:
+        raise error
+        getSteamAcc.reset_cooldown(ctx)
+        
+@client.command()
 async def setLineCount(ctx, *, number):
     global lineCount
     global con
